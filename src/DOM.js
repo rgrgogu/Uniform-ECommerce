2f920e2568348fc7b264435603da07cb9b716ffc
lzynth0506 <kenbusano2003@gmail.com>
2022-12-29 19:19:45 +0800

    source images :heart:

---
+++ src/DOM.js
@@ -0,0 +1,10 @@
+const sidebar = document.querySelector("#sidebar"),
+      sidebarMenu = document.querySelector("#sidebar-menu"),
+      menu = document.querySelector("#menu")
+      bodyScroll = document.body
+
+menu.addEventListener("click", () => {
+	sidebar.classList.toggle("sm:hidden")
+    sidebarMenu.classList.toggle("sm:hidden")
+    bodyScroll.classList.toggle("sm:overflow-y-hidden")
+});
