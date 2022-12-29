e8a642e1135ec57c1f27d63315609763d1b5efdb
lzynth0506 <kenbusano2003@gmail.com>
2022-12-29 19:41:10 +0800

    PHP support for tailwind :heart:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{html,js}",
-    "**/*.{html,js}"
+    "./pages/**/*.{html,js,php}",
+    "**/*.{html,js,php}"
   ],
   theme: {
     screens: {
