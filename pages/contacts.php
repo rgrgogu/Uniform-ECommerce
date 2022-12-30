13c2558910aaae1ac32f090ff6b48aa0e7e96ede
lzynth0506 <kenbusano2003@gmail.com>
2022-12-31 02:33:24 +0800

    Add some more pages :yellow_heart:

---
+++ pages/contacts.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" /> 
+    <link href="../dist/main.css" rel="stylesheet" />
+    <title>Uniform | Contacts</title>
+</head>
+<body>
+    <header id="navbar-container">
+        <nav class="lg:container lg:mx-auto flex items-center justify-between sm:p-4 lg:p-6">
+            <div id="logo" class="sm:w-40 lg:w-60">
+                <a href="../">
+                    <img src="../src/assets/plm-logo--with-header.png" alt="">
+                </a>
+            </div>
+            <div id="search" class="w-96 sm:hidden lg:block">
+                <div class="input-group relative flex flex-nowrap items-stretch w-full">
+                  <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-tl-lg rounded-bl-lg transition ease-in-out m-0" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+                  <button class="btn px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-tr-lg rounded-br-lg flex items-center">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                    </svg>
+                  </button>
+                </div>
+            </div>
+            <div class="sm:hidden lg:flex lg:items-center">
+                <div id="login" class="lg:mr-6">
+                    <a href="../login.php">
+                        <button>Login</button>
+                    </a>
+                </div>
+                <div id="register" class="lg:mr-6">
+                    <a href="../register.php">
+                        <button>Register</button>
+                    </a>
+                </div>
+                <!-- <div id="cart" class="">
+                    <span class="material-symbols-outlined text-3xl cursor-pointer">
+                        shopping_bag
+                    </span>
+                </div> -->
+            </div>
+            <div id="menu" class="cursor-pointer lg:hidden">
+                <span class="material-symbols-outlined pointer-events-none">
+                    menu
+                </span>
+            </div>
+        </nav>
+        <aside id="sidebar" class="bg-blue-500 p-4 text-white sm:hidden">
+            <div id="search" class="w-full mb-4">
+                <div class="input-group relative flex flex-nowrap items-stretch w-full">
+                  <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-tl-lg rounded-bl-lg transition ease-in-out m-0" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+                  <button class="btn px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-tr-lg rounded-br-lg flex items-center">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                    </svg>
+                  </button>
+                </div>
+            </div>
+            <div id="register" class="sm:mb-2">
+                <a href="../register.php">
+                    <button>Register</button>
+                </a>
+            </div>
+            <div id="login" class="">
+                <a href="../login.php">
+                    <button>Log In</button>
+                </a>
+            </div>
+        </aside>
+        <aside id="sidebar-menu" class="bg-blue-600 p-4 text-white sm:hidden lg:block">
+            <section class="lg:container lg:mx-auto lg:flex lg:items-stretch lg:justify-around">
+                <div id="page1" class="hover:text-purple-900">
+                    <a href="../">
+                        <button>Home</button>
+                    </a>
+                </div>
+                <div id="page2" class="hover:text-purple-900">
+                    <a href="../">
+                        <button>Order Online</button>
+                    </a>
+                </div>
+                <div id="page3" class="hover:text-purple-900">
+                    <a href="">
+                        <button>Contact Us</button>
+                    </a>
+                </div>
+                <div id="page4" class="hover:text-purple-900">
+                    <a href="../pages/FAQ.php">
+                        <button>FAQs</button>
+                    </a>
+                </div>
+                <div id="page5" class="hover:text-purple-900">
+                    <a href="../pages/abouts.php">
+                        <button>About Us</button>
+                    </a>
+                </div>
+            </section>
+        </aside>
+    </header>
+
+    <main id="bulletin" class="sm:p-6 lg:p-12">
+        <aside class="lg:container lg:mx-auto bg-blue-500 sm:p-6 lg:p-12 rounded-xl text-white grid lg:grid-cols-2 sm:grid-cols-1 lg:gap-6">
+            <div id="left-panel">
+                <header class="mb-2">
+                    <h2 class="text-3xl font-bold mb-2">Get in touch</h2>
+                    <p class="mb-2">If you have any concerns, questions, or suggestions on our product offerings, you may fillout our feedback form.</p>
+                    <span>How can we help?</span>
+                </header>
+                <form>
+                    <input type="text" placeholder="Enter your name" class="p-3 px-4 rounded-lg mb-4 border-none w-full text-black" autocomplete="off" required />
+                    <input type="email" placeholder="Enter your email address" class="p-3 px-4 rounded-lg mb-4 border-none w-full text-black" autocomplete="off" required />
+                    <textarea id="w3review" class="border border-black p-3 px-4 rounded-lg mb-4 border-none w-full text-black" autocomplete="off" required>Feedbacks or concerns</textarea>
+                    <input type="submit" value="Submit" class="bg-purple-600 w-full p-3 rounded-lg text-white cursor-pointer font-bold hover:bg-blue-600 sm:mb-2" />
+                </form>
+            </div>
+            <div id="right-panel">
+                <div class="place-content-start">
+                    <div class="mb-2">
+                        <span class="lg:text-xl mb-2 block">For General Inquires and Other Concerns</span>
+                        <p class="lg:text-lg font-bold">(+63)908-859-7995</p>
+                        <p class="lg:text-lg font-bold">(+63)995-789-4536</p>
+                    </div>
+                    <div class="mb-2">
+                        <span class="lg:text-xl mb-2 block">For Online Order Concerns</span>
+                        <p class="lg:text-lg font-bold">(+63)925-899-1595</p>
+                    </div>
+                    <div>
+                        <span class="lg:text-xl mb-2 block">or via Email</span>
+                        <p class="lg:text-lg font-bold">(+63)908-859-7995</p>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    </main>
+
+    <script src="../src/DOM.js"></script>
+</body>
+</html>
